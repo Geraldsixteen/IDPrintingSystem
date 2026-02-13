bb87a030319b470164d31a59b6e2e693c416fcb5
Gerald C. Panis <geraldpanis20@gmail.com>
2026-02-13 14:26:40 +0800

    Initial commit with Dockerfile and .gitignore

---
+++ LocalAdminSystem/Config/database.php
@@ -0,0 +1,25 @@
+<?php
+
+// Get variables
+$host = "dpg-d5vnr7e3jp1c73cc3bag-a.oregon-postgres.render.com";
+$port = "5432";
+$db   = "id_printing_db";
+$user = "auto";
+$pass = "5NpiRI7p6ZF6xTUN2U4blfnoN5BDIzRJ";
+
+// DSN string for PostgreSQL
+$dsn = "pgsql:host=$host;port=$port;dbname=$db;sslmode=require";
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    // echo "Connected to PostgreSQL successfully!";
+} catch (PDOException $e) {
+    die("PostgreSQL connection failed: " . $e->getMessage());
+}
+?>
